ed5c57cc9c5a4c3a8a398b47d688a9256a9dc81b
Nixya <64989593+nyxpirientity@users.noreply.github.com>
2026-03-08 22:02:09 -0700

    Initial Commit

---
+++ Source/AggressiveAgonyPlugin.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using BepInEx;
+using Nyxpiri.ULTRAKILL.NyxLib;
+
+namespace Nyxpiri.ULTRAKILL.AggressiveAgony
+{
+    [BepInPlugin("com.nyxpiri.bepinex.plugins.ultrakill.aggressive-agony", "Aggressive Agony", "0.0.0.1")]
+    [BepInProcess("ULTRAKILL.exe")]
+    public class AggressiveAgonyPlugin : BaseUnityPlugin
+    {
+        protected void Awake()
+        {
+            Log.Initialize(Logger);
+
+            AggressiveAgony.Initialze();
+        }
+
+        protected void Start()
+        {
+        }
+
+        protected void Update()
+        {
+
+        }
+
+        protected void LateUpdate()
+        {
+
+        }
+    }
+}
